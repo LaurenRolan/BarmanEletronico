dc6f9eeb758a9df269fc411d93edccab4b2c8625
cdflion <44637g@gmail.com>
2016-06-28 13:35:19 -0300

    by gabriel
    lauren mandou

--- debounce.vhd
+++ debounce.vhd
@@ -9,7 +9,7 @@ entity debounce is
 port
 	(
 		CLK:	in std_logic;
-		sin: 	in std_logic;
+		sign: 	in std_logic;
 		reset:in std_logic;
 		sai: 	out std_logic
 	);
@@ -17,8 +17,8 @@ end debounce;
 
 architecture behav of debounce is
 begin
-	FF1: entity work.ffd port map(sin, CLK, s1, reset);
+	FF1: entity work.ffd port map(sign, CLK, s1, reset);
 	FF2: entity work.ffd port map(s1, CLK, s2, reset);
 	FF3: entity work.ffd port map(s2, CLK, s3, reset);
-	sai <= s1 and s2 and s3;
+	sai <= s1 and s2 and not s3;
 end behav;
